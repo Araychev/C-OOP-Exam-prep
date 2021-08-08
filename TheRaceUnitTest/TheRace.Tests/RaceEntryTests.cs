3dce1a4d40e856b5966637d20bf331da78d48346
Angel Raychev <angelraychev@gmail.com>
2021-08-08 11:04:28 +0300

    The Race Test NUnit Done

--- TheRaceUnitTest/TheRace.Tests/RaceEntryTests.cs
+++ TheRaceUnitTest/TheRace.Tests/RaceEntryTests.cs
@@ -1,5 +1,6 @@
+using System;
 using NUnit.Framework;
-using TheRace;
+using System.Collections.Generic;
 
 namespace TheRace.Tests
 {
@@ -9,12 +10,62 @@ namespace TheRace.Tests
         [SetUp]
         public void Setup()
         {
+            UnitDriver driver = new UnitDriver("Ivan", new UnitCar("Fiat", 150, 1900));
+            Dictionary<string, UnitDriver> drivers = new Dictionary<string, UnitDriver>();
         }
 
         [Test]
-        public void TestOne()
+        public void AddDriver_ThrowExceptionWhenNullisPassed()
         {
-            Assert.Pass();
+            var raceEntry = new RaceEntry();
+
+           Assert.Throws<InvalidOperationException>
+               ((() => raceEntry.AddDriver(null)));
+        }
+        [Test]
+        public void AddDriver_ThrowExceptionWhenDriverExist()
+        {
+            var raceEntry = new RaceEntry();
+            UnitDriver driver = new UnitDriver("Ivan", new UnitCar("Fiat", 150, 1900));
+            raceEntry.AddDriver(driver);
+
+           Assert.Throws<InvalidOperationException>
+               ((() => raceEntry.AddDriver(driver)));
+           Assert.AreEqual(1,raceEntry.Counter);
+        }
+
+        [Test]
+        public void CalculateAverageHorsePower_ThrowExceptionWhenDriverCountIsSmallerThanMinParticipant()
+        {
+            int MinParticipants = 2;
+            var raceEntry = new RaceEntry();
+            Dictionary<string, UnitDriver> driver = new Dictionary<string, UnitDriver>();
+
+           Assert.Throws<InvalidOperationException>
+               ((() => raceEntry.CalculateAverageHorsePower()));
+        }
+
+        [Test]
+        public void CalculateAverageHorsePower()
+        {
+            
+            var raceEntry = new RaceEntry();
+
+            UnitDriver driver = new UnitDriver("Ivan", new UnitCar("Fiat", 100, 1900));
+            UnitDriver driver1 = new UnitDriver("Dragan", new UnitCar("BMW", 100, 2000));
+            UnitDriver driver2 = new UnitDriver("Miro", new UnitCar("Lada", 100, 1500));
+
+
+            raceEntry.AddDriver(driver);
+            raceEntry.AddDriver(driver1);
+            raceEntry.AddDriver(driver2);
+           
+            var actual = raceEntry.CalculateAverageHorsePower();
+                
+
+            Assert.AreEqual(100, actual);
+
+         
         }
     }
 }
