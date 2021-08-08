243100bfa7f63fe8d0cf5ff357146506e7c8a0c0
Angel Raychev <angelraychev@gmail.com>
2021-08-08 13:10:35 +0300

    Festival Manager Unit Test Done!

---
+++ FestivalManagerUnitTest/FestivalManager.Tests/StageTests.cs
@@ -0,0 +1,95 @@
+// Use this file for your unit tests.
+// When you are ready to submit, REMOVE all using statements to Festival Manager (entities/controllers/etc)
+// Test ONLY the Stage class. 
+
+//using System.Collections.Generic;
+//using FestivalManager.Entities;
+
+namespace FestivalManager.Tests
+{
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+	public class StageTests
+    {
+        private Stage stage;
+
+        [SetUp]
+        public void Setup()
+        {
+            stage = new Stage();
+        }
+
+		[Test]
+	    public void AddPerformer_ThrowException()
+        {
+            Performer performer = new Performer("Ivan", "Tolev", 17);
+
+            Assert.Throws<ArgumentException>((() => stage.AddPerformer(performer)));
+        }
+
+		[Test]
+	    public void AddPerformer_Success()
+        {
+            Performer performer = new Performer("Ivan", "Tolev", 37);
+
+            stage.AddPerformer(performer);
+          
+        }
+
+        [Test]
+        public void AddSong_ThrowException()
+        {
+            Song song = new Song("Lambada", TimeSpan.Zero);
+
+            Assert.Throws<ArgumentException>((() => stage.AddSong(song)));
+        }
+
+        [Test]
+        public void AddSong_Success()
+        {
+            Song song = new Song("Lambada", TimeSpan.FromMinutes(30));
+
+           stage.AddSong(song);
+        }
+
+        [Test]
+        public void AddSongToPerformer_Success()
+        {
+            var performer = new Performer("Ivan", "Ivanov", 19);
+            var song = new Song("Ветрове", new TimeSpan(0,3,30));
+            stage.AddSong(song);
+            stage.AddPerformer(performer);
+           
+
+            string expected = "Ветрове (03:30) will be performed by Ivan Ivanov";
+            string actual = stage.AddSongToPerformer("Ветрове", "Ivan Ivanov");
+
+          Assert.AreEqual(expected,actual);
+
+        }
+
+        [Test]
+        public void Play_Success()
+        {
+
+            var performer = new Performer("Ivan", "Ivanov", 19);
+            var song = new Song("Ветрове", new TimeSpan(0,3,30));
+            stage.AddSong(song);
+            stage.AddPerformer(performer);
+            stage.AddSongToPerformer("Ветрове", "Ivan Ivanov");
+           
+
+            string expected = "1 performers played 1 songs";
+            string actual = stage.Play();
+
+          Assert.AreEqual(expected,actual);
+
+        }
+
+
+
+
+	}
+}
---
+++ PresentsUnitTest/Presents.Tests/PresentsTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Presents.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PresentsTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [Test]
+        public void Create_ThrowExeptionWhenIsNull()
+        {
+        }
+    }
+}
