5d1b1f7e6b60bee3d367e123ea6e847f64c8fa8b
Angel Raychev <angelraychev@gmail.com>
2021-08-13 13:04:13 +0300

    C# OOP Retake Exam – 18 April 2021

---
+++ Easter/Easter/Repositories/BunnyRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Easter.Models.Bunnies.Contracts;
+using Easter.Repositories.Contracts;
+
+namespace Easter.Repositories
+{
+    public class BunnyRepository : IRepository<IBunny>
+    {
+        private readonly List<IBunny> _bunnies;
+        public BunnyRepository()
+        {
+            _bunnies = new List<IBunny>();
+        }
+
+        public IReadOnlyCollection<IBunny> Models { get; private set; } =  new List<IBunny>();
+        public void Add(IBunny model)
+        {
+            _bunnies.Add(model);
+            Models = _bunnies;
+        }
+
+        public bool Remove(IBunny model)
+        {
+            if (_bunnies.Contains(model))
+            {
+                _bunnies.Remove(model);
+                Models = _bunnies;
+                return true;
+            }
+
+            return false;
+        }
+
+        public IBunny FindByName(string name)
+        {
+            return _bunnies.FirstOrDefault(x => x.Name == name);
+        }
+    }
+}
---
+++ Easter/Easter/Repositories/EggRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Easter.Models.Eggs.Contracts;
+using Easter.Repositories.Contracts;
+
+namespace Easter.Repositories
+{
+   public class EggRepository : IRepository<IEgg>
+   {
+       private readonly List<IEgg> _eggs;
+        public EggRepository()
+        {
+            _eggs = new List<IEgg>();
+        }
+
+        public IReadOnlyCollection<IEgg> Models { get; private set; } = new List<IEgg>();
+        public void Add(IEgg model)
+        {
+            _eggs.Add(model);
+            Models = _eggs;
+
+        }
+
+        public bool Remove(IEgg model)
+        {
+            if (_eggs.Contains(model))
+            {
+                _eggs.Remove(model);
+                Models = _eggs;
+                return true;
+            }
+
+            return false;
+        }
+
+        public IEgg FindByName(string name)
+        {
+                return _eggs.FirstOrDefault(b => b.Name == name);
+        }
+        
+    }
+}
